42f47f2292366c646ae4149e9fd7b918ebba0154
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-17 23:30:44 -0500

    Added separate file for black jack functions

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
-from art import logo
-import os
+from art import *
+from blackjack_functions import *
 import random
+# from blackjack_fun import clear
 ############### Blackjack Project #####################
 
 #Difficulty Normal 😎: Use all Hints below to complete the project.
@@ -27,42 +28,6 @@ player_hand = random.sample(cards,2)
 player_score = sum(player_hand)
 dealers_hand = random.sample(cards,2)
 dealers_score = sum(dealers_hand)
-
-def clear():
-    os.system("cls")
-    os.system("clear")
-
-def print_cards(player_hand, player_score, dealers_hand):
-    print(f"Your cards: {player_hand}, current score: {player_score}")
-    print(f"Dealer's first card: {dealers_hand[0]}")
-
-def check_win(player_hand, player_score, dealers_hand, dealers_score):
-    if dealers_score < 17:
-        dealers_hand.append(random.choice(cards))
-        dealers_score = sum(dealers_hand)
-    print(f"Your final hand: {player_hand}, final score: {player_score}")
-    print(f"Dealer's final hand: {dealers_hand}, final score: {dealers_score}")
-    if dealers_score and player_score <= 21:
-        if dealers_score == player_score:
-            print("Tie!")
-        else:
-            print("Dealer won!") if dealers_score > player_score else print("Player won!")
-    elif dealers_score > 21:
-        print("Dealer has went over 21, Player won!")
-    else:
-        print("Player has went over 21, Dealer won!")
-
-def draw_card(choice, player_hand, player_score, dealers_hand, dealers_score):
-    if choice == "y":
-        player_hand.append(random.choice(cards))
-        player_score = sum(player_hand)
-        if dealers_score < 17:
-            dealers_hand.append(random.choice(cards))
-            dealers_score = sum(dealers_hand)
-        print_cards(player_hand, player_score, dealers_hand)
-        check_win(player_hand, player_score, dealers_hand, dealers_score)
-    else:
-        check_win(player_hand, player_score, dealers_hand, dealers_score)
 
 if play_game_choice == "y":
     clear()
---
+++ blackjack_functions.py
@@ -0,0 +1,38 @@
+import os
+import random
+# from blackjack import cards
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def clear():
+    os.system("cls")
+    os.system("clear")
+
+def print_cards(player_hand, player_score, dealers_hand):
+    print(f"Your cards: {player_hand}, current score: {player_score}")
+    print(f"Dealer's first card: {dealers_hand[0]}")
+
+def check_win(player_hand, player_score, dealers_hand, dealers_score):
+    if dealers_score < 17:
+        dealers_hand.append(random.choice(cards))
+        dealers_score = sum(dealers_hand)
+    print(f"Your final hand: {player_hand}, final score: {player_score}")
+    print(f"Dealer's final hand: {dealers_hand}, final score: {dealers_score}")
+    if dealers_score <= 21 and player_score <= 21:
+        if dealers_score == player_score:
+            print("Tie!")
+        else:
+            print("Dealer won!") if dealers_score > player_score else print("Player won!")
+    elif dealers_score > 21:
+        print("Both have lost in a Tie!") if player_score > 21 else print("Dealer has went over 21, Player won!")
+    else:
+        print("Player has went over 21, Dealer won!")
+
+def draw_card(choice, player_hand, player_score, dealers_hand, dealers_score):
+    if choice == "y":
+        player_hand.append(random.choice(cards))
+        player_score = sum(player_hand)
+        print_cards(player_hand, player_score, dealers_hand)
+        check_win(player_hand, player_score, dealers_hand, dealers_score)
+    else:
+        check_win(player_hand, player_score, dealers_hand, dealers_score)
