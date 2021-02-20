e67998577b59ceddf631f8552c4981e4555e401c
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-20 12:50:13 -0500

    Added option to play again

--- blackjack.py
+++ blackjack.py
@@ -28,11 +28,19 @@ player_score = sum(player_hand)
 dealers_hand = random.sample(cards,2)
 dealers_score = sum(dealers_hand)
 
-if play_game_choice == "y":
-    clear()
-    print(logo)
-    print_cards(player_hand, player_score, dealers_hand)
-    draw_card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
-    draw_card(draw_card_choice, player_hand, player_score, dealers_hand, dealers_score)
-else:
-    exit()
+def blackjack(player_hand, player_score, dealers_hand, dealers_score):
+    if play_game_choice == "y":
+        clear()
+        print(logo)
+        print_cards(player_hand, player_score, dealers_hand)
+        draw_card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+        draw_card(draw_card_choice, player_hand, player_score, dealers_hand, dealers_score)
+        continue_choice = input("Play again? Type 'y' or 'n': ").lower()
+        if continue_choice == "y":
+            blackjack(player_hand, player_score, dealers_hand, dealers_score)
+        else:
+            exit()
+    else:
+        exit()
+
+blackjack(player_hand, player_score, dealers_hand, dealers_score)
