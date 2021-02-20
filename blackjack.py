08b75e7a429b9d1cce7bd3ad563b5c21081393f1
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-20 14:33:06 -0500

    Removed comments

--- blackjack.py
+++ blackjack.py
@@ -1,27 +1,7 @@
 from art import *
 from blackjack_functions import *
 import random
-# from blackjack_fun import clear
-############### Blackjack Project #####################
 
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
 def blackjack():
     play_game_choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     player_hand = random.sample(cards,2)
@@ -40,5 +20,4 @@ def blackjack():
     else:
         exit()
 
-# Truthy or Falsy instead
 blackjack()
